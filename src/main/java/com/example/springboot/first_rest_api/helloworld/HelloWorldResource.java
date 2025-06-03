7ca28010fd90d6505f5b897fcf30e297c104f40c
bhshenoy <bharathshenoy.1rn17cs026@gmail.com>
2025-06-03 09:53:42 +0530

    Understand path param, path variable

--- src/main/java/com/example/springboot/first_rest_api/helloworld/HelloWorldResource.java
+++ src/main/java/com/example/springboot/first_rest_api/helloworld/HelloWorldResource.java
@@ -1,5 +1,6 @@
 package com.example.springboot.first_rest_api.helloworld;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,21 @@ public class HelloWorldResource {
     // This bean is returned as a JSON object using jackson
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello world");
+    }
+
+    // need to add a custom value and return it
+    // ex: /hello-world-path-param/Ram will give json response as "Hello World, Ram"
+    // {name} -> Path param, @PathVariable String name -> path variable
+    @RequestMapping("hello-world-path-param/{name}")
+    public HelloWorldBean helloWorldBeanPathParam(@PathVariable String name) {
+        return new HelloWorldBean("Hello world, "+name);
+    }
+
+    // Ex: /hello-world-path-param/Hanuman/message/Jai Shree Ram
+    @RequestMapping("hello-world-path-param/{name}/message/{message}")
+    public HelloWorldBean helloWorldBeanMultiPathParam(@PathVariable String name,
+                                                       @PathVariable String message) {
+        return new HelloWorldBean("Hello world, "+name+", Message passed: "+message);
     }
 
 }
