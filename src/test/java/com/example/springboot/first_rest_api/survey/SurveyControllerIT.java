75997adf6f00d77a9f7d630dedd89fa8950f6c58
bhshenoy <bharathshenoy.1rn17cs026@gmail.com>
2025-06-15 17:00:36 +0530

    Springboot IT -> IT for POST question, use exchange method to fire request, note down observation

--- src/test/java/com/example/springboot/first_rest_api/survey/SurveyControllerIT.java
+++ src/test/java/com/example/springboot/first_rest_api/survey/SurveyControllerIT.java
@@ -6,6 +6,9 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -28,19 +31,7 @@ class SurveyControllerIT {
      * Fire a request to http://localhost:8080/surveys/survey1/questions/question1
      * and assert on the expectedResponse
      */
-    String expectedResponse = """
-            {
-                "id": "Question1",
-                "description": "Most Popular Cloud Platform Today",
-                "options": [
-                "AWS",
-                "Azure",
-                "Google Cloud",
-                "Oracle Cloud"
-                ],
-                "correctAnswer": "AWS"
-            }
-            """;
+
     // Header: [content-type:"application/json", date:"Sat, 14 Jun 2025 17:39:48 GMT", transfer-encoding:"chunked"]
     // Body: {"id":"Question1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"correctAnswer":"AWS"}
     // GET http://localhost:RANDOM_PORT/surveys/survey1/questions/question1
@@ -95,5 +86,41 @@ class SurveyControllerIT {
     }
 
     // TODO: IT for GET /surveys and GET /surveys/survey1
+
+    /**
+     * POST /surveys/{surveyId}/questions
+     * http:localhost:RANDOM_PORT/surveys/survey/questions/question4
+     * Request header ->  Content-Type: application/json
+     * Request body -> given as string above
+     * Assert on -> status code, location header (http:localhost:RANDOM_PORT/surveys/survey/questions/AUTO_GENERATED_ID)
+     */
+    @Test
+    void addNewSurveyQuestion_basicScenario() {
+
+        // Prepare request header
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-Type", "application/json");
+
+        // Request body
+        String requestBody = """
+            {
+                "description": "Fastest programming language",
+                "options": ["Java","Python","C++","JavaScript"],
+                "correctAnswer": "C++"
+            }
+            """;
+
+        // Combine both request header and body
+        HttpEntity<String> httpEntity = new HttpEntity<String>(requestBody, httpHeaders);
+
+        // fire the request
+        ResponseEntity<String> responseEntity =
+                testRestTemplate.exchange(ALL_QUESTION_URLS, HttpMethod.POST, httpEntity, String.class);
+
+//        Response Header: [content-length:"0", date:"Sun, 15 Jun 2025 11:20:06 GMT", location:"http://localhost:55749/surveys/survey1/questions/1214278348"]
+
+        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+        assertTrue(responseEntity.getHeaders().get("Location").get(0).contains("/surveys/survey1/questions"));
+    }
 
 }
