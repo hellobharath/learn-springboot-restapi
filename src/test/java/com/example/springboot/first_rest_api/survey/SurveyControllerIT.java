c6afc3357c07572ff8321a5f56e42265653eb65c
bhshenoy <bharathshenoy.1rn17cs026@gmail.com>
2025-06-15 16:22:03 +0530

    Springboot integration test (IT) -> Adding 1 more IT for GET list, add relevant asserts for status and content type

--- src/test/java/com/example/springboot/first_rest_api/survey/SurveyControllerIT.java
+++ src/test/java/com/example/springboot/first_rest_api/survey/SurveyControllerIT.java
@@ -6,14 +6,23 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // This annotation launches spring application context, parameter assigns random port
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SurveyControllerIT {
+
+    // Autowiring takes care of http://localhost:RANDOM_PORT
+    // TestRestTemplate -> library class to handle rest request hit
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    // Now effective endpoint is /surveys/survey1/questions/question1
+    private static final String SPECIFIC_QUESTION_URL = "/surveys/survey1/questions/question1";
+    private static final String ALL_QUESTION_URLS = "/surveys/survey1/questions";
 
     /**
      * Fire a request to http://localhost:8080/surveys/survey1/questions/question1
@@ -32,16 +41,11 @@ class SurveyControllerIT {
                 "correctAnswer": "AWS"
             }
             """;
-    // Autowiring takes care of http://localhost:RANDOM_PORT
-    @Autowired
-    private TestRestTemplate testRestTemplate;
-
-    // Now effective endpoint is /surveys/survey1/questions/question1
-    private static String SPECIFIC_QUESTION_URL = "/surveys/survey1/questions/question1";
     // Header: [content-type:"application/json", date:"Sat, 14 Jun 2025 17:39:48 GMT", transfer-encoding:"chunked"]
     // Body: {"id":"Question1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"correctAnswer":"AWS"}
+    // GET http://localhost:RANDOM_PORT/surveys/survey1/questions/question1
     @Test
-    void retrieveQuestionsForSurvey_basicScenario() throws JSONException {
+    void retrieveSpecificQuestionForSurvey_basicScenario() throws JSONException {
         // Arrange -> not required here since we are firing a get request, and endpoint defined above
         String expectedResponseBody = """
                 {
@@ -53,11 +57,43 @@ class SurveyControllerIT {
         // Act -> firing request
         ResponseEntity<String> responseEntity = testRestTemplate.getForEntity(SPECIFIC_QUESTION_URL, String.class);
 
-        System.out.println("Header: "+ responseEntity.getHeaders());
-        String responseBody = responseEntity.getBody();
-        System.out.println("Body: "+ responseBody);
-        // Assert
-//        assertEquals(expectedResponse.trim(), responseBody);
-        JSONAssert.assertEquals(expectedResponseBody, responseBody, false);
+        // Assert status of response = 200
+        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+
+        // Assert content type which is sent as part of header
+        assertEquals("application/json", responseEntity.getHeaders().get("content-type").get(0));
+
+        // Assert response body
+        JSONAssert.assertEquals(expectedResponseBody, responseEntity.getBody(), false);
     }
+
+    // GET http://localhost:RANDOM_PORT/surveys/survey1/questions
+    @Test
+    void retrieveAllQuestionsForSurvey_basicScenario() throws JSONException {
+        String expectedResponseBody = """
+                [
+                    {
+                        "id":"Question1",
+                        "correctAnswer":"AWS"
+                    },
+                    {
+                        "id":"Question2"
+                    },
+                    {
+                        "id":"Question3",
+                        "correctAnswer": "Kubernetes"
+                    }
+                ]
+                """;
+
+        ResponseEntity<String> responseEntity = testRestTemplate.getForEntity(ALL_QUESTION_URLS, String.class);
+
+        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+        assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+        JSONAssert.assertEquals(expectedResponseBody, responseEntity.getBody(), false);
+
+    }
+
+    // TODO: IT for GET /surveys and GET /surveys/survey1
+
 }
